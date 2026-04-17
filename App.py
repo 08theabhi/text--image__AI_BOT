6af6c61413f7a0cbb7f6fe629f0c240196b28396
Abhishek H N <1NC25MC001@ncetmail.com>
2026-04-17 21:26:55 +0530

    Fix API_URL assignment in App.py

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ import requests
 from PIL import Image
 import io
 
-API_URL = "API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0""
+API_URL = "API_URL = https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
 HEADERS = {"Authorization": "Bearer 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"}  # your HF token here
 
 def generate_image(prompt):
